8e530930e76afd0d11beee2b040fd9451419baed
Alec Georgoff <alec.georgoff@gmail.com>
2021-08-31 17:41:05 -0700

    Added react-bootstrap and set up a container.

--- spotify-album-art/src/App.tsx
+++ spotify-album-art/src/App.tsx
@@ -3,7 +3,7 @@ import { TestApiResults } from './TestApiResults';
 
 function App() {
     return (
-        <div className="App">
+        <div className="container">
             <header className="App-header">
                 <TestApiResults />
             </header>
