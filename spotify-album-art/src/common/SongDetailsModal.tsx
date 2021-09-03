839508aa474e2ecdeac2afe5ebf20314cc9cc4ec
Alec Georgoff <alec.georgoff@gmail.com>
2021-09-03 15:27:47 -0700

    Added song details to open modal.

--- spotify-album-art/src/common/SongDetailsModal.tsx
+++ spotify-album-art/src/common/SongDetailsModal.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Modal } from 'react-bootstrap';
 import { getArtistById } from '../api/SpotifyApi';
-import { GetImage } from '../api/SpotifyHelpers';
+import { GetImage, ListWithCommas } from '../api/SpotifyHelpers';
 import { SpotifyArtist, SpotifyTrack } from '../types/SpotifyTypes';
 
 interface Props {
@@ -33,12 +33,17 @@ export const SongDetailsModal = (props: Props) => {
                         />
                     </Modal.Header>
                     <Modal.Body>
-                        <div>
+                        <div className="open-modal-song-details">
                             <img
                                 src={GetImage(props.song.album.images, 'large')}
                                 alt={props.song.name}
-                                className="song-details-album-image"
                             />
+                            <div>
+                                <h4>{props.song.name}</h4>
+                                <h5>
+                                    {ListWithCommas(props.song.artists.map(artist => artist.name))}
+                                </h5>
+                            </div>
                         </div>
                     </Modal.Body>
                 </>
