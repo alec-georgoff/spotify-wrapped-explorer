498cb87070bccf0438a392c967e02ed2045a813a
Alec Georgoff <alec.georgoff@gmail.com>
2021-09-03 12:59:42 -0700

    Added artist image to modal.

--- spotify-album-art/src/common/SongDetailsModal.tsx
+++ spotify-album-art/src/common/SongDetailsModal.tsx
@@ -1,5 +1,8 @@
+import { useEffect, useState } from 'react';
 import { Modal } from 'react-bootstrap';
-import { SpotifyTrack } from '../types/SpotifyTypes';
+import { getArtistById } from '../api/SpotifyApi';
+import { GetImage } from '../api/SpotifyHelpers';
+import { SpotifyArtist, SpotifyTrack } from '../types/SpotifyTypes';
 
 interface Props {
     song: SpotifyTrack;
@@ -7,8 +10,30 @@ interface Props {
     onClose: () => void;
 }
 
-export const SongDetailsModal = (props: Props) => (
-    <Modal show={props.open} onHide={props.onClose}>
-        <Modal.Header>Put artist photo here</Modal.Header>
-    </Modal>
-);
+export const SongDetailsModal = (props: Props) => {
+    const [artistDetails, setArtistDetails] = useState<SpotifyArtist>();
+
+    useEffect(() => {
+        if (props.open) {
+            getArtistById(props.song.artists[0].id).then(data => setArtistDetails(data));
+        } else {
+            setArtistDetails(undefined);
+        }
+    }, [props.open, props.song.artists]);
+
+    return (
+        <Modal show={props.open} onHide={props.onClose}>
+            {artistDetails ? (
+                <Modal.Header>
+                    <img
+                        src={GetImage(artistDetails.images, 'large')}
+                        alt={artistDetails.name}
+                        className="song-details-artist-image"
+                    />
+                </Modal.Header>
+            ) : (
+                <Modal.Header>Error loading artist info</Modal.Header>
+            )}
+        </Modal>
+    );
+};
